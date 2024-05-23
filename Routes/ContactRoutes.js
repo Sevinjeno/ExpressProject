7cdaebdcf7e57aaf584323d8c73199731ca3d035
Sevinjeno <7jenodev@gmail.com>
2024-05-23 15:08:04 +0530

    Controller Implmented

--- Routes/ContactRoutes.js
+++ Routes/ContactRoutes.js
@@ -1,24 +1,11 @@
 const express=require("express");
 const router=express.Router();
+const {getContact,createContact,updateContact,deleteContact}=require("../controllers/ContactControllers")
 
 //this is the way to get the results for every route 
 
-router.route("/").get((req,res)=>{
-    res.status(200).json({message:"Get all Contacts"});
-})
+router.route("/").get(getContact).post(createContact)
 
-router.route("/").post((req,res)=>{
-    res.status(200).json({message:"Create Contacts"});
-})
-
-router.route("/:id").put((req,res)=>{
-    res.status(200).json({message:`Update for ${req.params.id}`});
-})
-
-router.route("/:id").delete((req,res)=>{
-    res.status(200).json({message:`Delete for ${req.params.id}`});
-})
-
-
+router.route("/:id").put(updateContact).delete(deleteContact)
 
 module.exports=router;   //no need to define module , we can directly use module to export
